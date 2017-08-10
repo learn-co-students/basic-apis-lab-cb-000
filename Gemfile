09c895f9dcb49c95c8b200b3fd50e86a366abdff
Drew Pham <drewvpham@gmail.com>
2017-08-09 19:13:09 -0500

    done

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 
 
+gem 'faraday'
+gem 'hirb'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
 # Use sqlite3 as the database for Active Record
--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,25 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    begin
+       @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+           req.params['client_id'] = "e1f888c726f9f70a278d"
+           req.params['client_secret'] = "51b79f7c1be71eade3d94abb8b966fb6d74ea711"
+
+           req.params['q'] = params[:query]
+
+           req.options.timeout = 100
+         end
+         body = JSON.parse(@resp.body)
+         if @resp.success?
+           @results = body['items']
+         else
+           @error = 'Missing query search'
+         end
+
+       rescue Faraday::ConnectionFailed
+         @error = "There was a timeout. Please try again."
+       end
+       render 'search'
+     end
   end
-end
