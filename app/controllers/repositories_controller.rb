2bcfd4b6cc86a5447dc674efea5889dd5982a1d0
youssefmys <mosa500@gmail.com>
2018-04-11 02:24:58 +0200

    Done

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,26 @@
 class RepositoriesController < ApplicationController
 
   def search
-
+    render :search
   end
 
   def github_search
+    begin
+      @resp = Faraday.get 'https://api.github.com/search/repositories'  do |req|
+        req.params['q'] = params[:query]
+      end
+
+      if !@resp.success?
+        @error = "connection filed  !@resp.success"
+      else
+        @items_array = JSON.parse(@resp.body)["items"]
+      end
+
+    rescue Faraday::ConnectionFailed
+      @error = 'connection failed'
+    end
+
+    render :search
   end
+
 end
