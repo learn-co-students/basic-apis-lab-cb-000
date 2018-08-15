53dedbc6398ba905ee920ad914dc0ca6d2f6bf1d
Justin Sinn <justin.sinn.co@gmail.com>
2018-08-15 00:05:44 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,3 +1,5 @@
+
+
 class RepositoriesController < ApplicationController
 
   def search
@@ -5,5 +7,18 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['q']= 'recurly'
+      req.params['client_id']= 'Iv1.6b48ace725584892'
+      req.params['client_secret']= '15c5bac93ab26709539ba2cc9de7e37a33b34cb8'
+    end
+
+    body_hash = JSON.parse(@resp.body)
+    @repositories = body_hash["items"]
+
+  render 'search'
+
   end
+
+
 end
